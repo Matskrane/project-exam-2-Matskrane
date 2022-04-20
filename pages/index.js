686113f53df3af49b88944c2e68d428073990cbc
Matskrane <69842790+Matskrane@users.noreply.github.com>
2022-04-20 12:42:20 +0200

    styling

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
+import { HiWifi } from "react-icons/hi";
+import { MdOutlineStar, MdQuickreply } from "react-icons/md";
 import HeroBanner from "../components/heroBanner/HeroBanner";
+
 
 
 
 export default function Home() {
   return (
     <>
-    <HeroBanner />
-    <h2>Cheap and Fast Service</h2>
+      <HeroBanner />
+      
+      <div className="section-one">
+        <div className="container-one">
+          <h2>Why should you book with us  ?</h2>
+          <div className="flexbox-one">
+            <div className="section-icons">
+              <HiWifi />
+            <p>Free Wi-Fi at all of our hotels</p>
+            </div>
+            <div className="section-icons">
+              <MdOutlineStar />
+            <p>Great reviews!</p>
+            </div>
+            <div className="section-icons">
+              <MdQuickreply /> 
+            <p>Quick and easy ff15</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
